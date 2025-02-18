419bc0b3abab341e7ac3c59d881ab88afee7409d
s4kr3d-w0r1d <saksham.madan.oct20@gmail.com>
2025-02-18 23:33:28 +0530

    Initial commit - project setup

---
+++ whatsapp-clone/src/test/java/com/whatsappclone/whatsapp_clone/WhatsappCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatsappclone.whatsapp_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
