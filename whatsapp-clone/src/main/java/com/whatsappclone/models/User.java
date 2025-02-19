ec505b393066c4a442ada4cbb624ccc7dd3a2474
s4kr3d-w0r1d <saksham.madan.oct20@gmail.com>
2025-02-19 13:45:25 +0530

    Added User Class where all properites of user are defined . Added inteface User repository to find user via email address. created users table in SQL

---
+++ whatsapp-clone/src/main/java/com/whatsappclone/models/User.java
@@ -0,0 +1,25 @@
+package com.whatsappclone.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+}
