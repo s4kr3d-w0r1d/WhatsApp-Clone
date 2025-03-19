1fdccae06a22db7e7e35adf61cd8eefc4ed50356
s4kr3d-w0r1d <saksham.madan.oct20@gmail.com>
2025-03-19 23:50:56 +0530

    fixed endpoints and changed services a bit to improve the backend and frontend compatibility .
    now group can have a group dp

---
+++ whatsapp-clone/src/main/java/com/whatsappclone/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.whatsappclone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:C:/uploads/"); // Change path if needed
+    }
+}
